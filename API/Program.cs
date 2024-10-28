5d4014cbbdedeb2f6cce2743fc72004a9f215efe
jkushwaha <singh.jitendra18@gmail.com>
2024-10-28 08:12:51 +0530

    End of Section 3

--- API/Program.cs
+++ API/Program.cs
@@ -10,9 +10,13 @@ builder.Services.AddDbContext<DataContext>(opt=>
 {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddCors();
+
 var app = builder.Build();
 
-
+app.UseCors(x=>x.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod()
+    .WithOrigins("http://localhost:4200","https://localhost:4200"));
+    
 app.MapControllers();
 
 app.Run();
